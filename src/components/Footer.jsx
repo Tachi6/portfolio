eb584aaa489d379657533f6898a47c0b507c61f2
David Gonzalez <dav6wat@gmail.com>
2025-10-31 16:06:18 +0100

    init footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,25 @@
+import { Link } from 'react-router';
+
 export const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div>
+        <p>
+          Design inspired in{' '}
+          <Link to="https://www.figma.com/design/FkD7mvAN9yA2DgMXHSxl9c/Portfolio?m=auto&t=tXDZQODDBsE8oBdp-6">
+            Portfolio
+          </Link>
+          <span>|</span>
+        </p>
+        <p>
+          Powered by
+          <Link to="https://react.dev/" target="_blank">
+            React
+          </Link>
+          <span>|</span>
+        </p>
+      </div>
+      <p class="copyright">&copy; 2025 David Gonzalez de la Rosa</p>
+    </footer>
+  );
 };
