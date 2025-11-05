c71f17f11893f61a59581cffc95a1d8add874ab1
David Gonzalez <dav6wat@gmail.com>
2025-11-05 15:05:59 +0100

    aboutme texts changes

--- src/data/aboutMeData.jsx
+++ src/data/aboutMeData.jsx
@@ -4,7 +4,7 @@ export const aboutMeData = [
   {
     label: 'Que estoy buscando',
     content:
-      'Estoy buscando mi primera oportunidad como Desarrollador Front-End Junior. Mi objetivo es iniciar mi carrera en el sector tecnológico, poniendo en práctica mi formación reciente y mi ética de trabajo rigurosa adquirida en el sector óptico, para crecer profesionalmente. Busco aprender de otros desarrolladores, profundizar en las tecnologías web, entender las metodologias de trabajo profesionales y colaborar en la creación o expansión de proyectos.',
+      'Estoy buscando mi primera oportunidad como Desarrollador Front-End Junior. Mi objetivo es iniciar mi carrera en el sector tecnológico, poniendo en práctica mi formación y mi ética de trabajo rigurosa adquirida en el sector óptico, para crecer profesionalmente. Busco aprender de otros desarrolladores, profundizar en las tecnologías web, entender las metodologias de trabajo profesionales y colaborar en la creación o expansión de proyectos.',
   },
   {
     label: 'Qué puedo aportar',
@@ -17,11 +17,11 @@ export const aboutMeData = [
         aprendizaje para encontrar soluciones válidas y efectivas.
         <br />
         <br />
-        Mi experiencia sanitaria previa me ha dotado de habilidades que puedo aportar en un equipo de trabajo. La
+        Mi experiencia sanitaria previa me ha dotado de habilidades que puedo aportar a un equipo de trabajo. La
         meticulosidad y atención al detalle me permite traducir diseños a Pixel Perfect con código limpio, estructurado
-        y escalable. La interacción con los usuarios me permite entender como crear diseños elegante, intuitivos y
-        accesibles. La evaluación de síntomas y el diagnóstico para prescribir soluciones visuales me ayuda a
-        identificar errores de código y solucionarlos de manera eficiente.
+        y escalable. La interacción con los usuarios me permite entender cuales son las necesidades de los proyectos
+        para crear diseños elegantes, intuitivos y accesibles. La evaluación de síntomas y el diagnóstico para
+        prescribir soluciones visuales me ayuda a identificar errores de código y solucionarlos de manera eficiente.
       </>
     ),
   },
@@ -29,26 +29,26 @@ export const aboutMeData = [
     label: 'Cambio laboral',
     content: (
       <>
-        Después de muchos años en el mundo de la óptica y viendo que no llenaba mis expectativas laborales, he decidido
-        dar el paso a la que pienso que siempre ha sido mi gran pasión, la informática, y más concretamente la
-        programación.
+        Después de muchos años en un mundo estático y sin apenas evolución como es la óptica, he decidido dar el paso a
+        la que pienso que siempre ha sido mi gran pasión, la informática, y más concretamente la programación, muy
+        dinámica y en evolucíon constante.
         <br />
         <br />
         Desde niño siempre me ha apasionado el mundo de los ordenadores y como de un simple código mostraba resultados
-        en pantalla. Recuerdo aquel primer MS-DOS, con el que investigué todos sus comandos, pasando por casi todos los
-        Windows, donde aprendí cientos de cosas, hasta que llegó a mi casa la primera conexión a internet y se abrió un
-        nuevo mundo de conocimientos.
+        en pantalla. Recuerdo aquel primer MS-DOS con el que investigué todos sus comandos, todas las versiones de
+        Windows con las que aprendí cientos de cosas y cuando llegó a mi casa la primera conexión a internet y descubrí
+        un nuevo mundo de conocimientos.
         <br />
         <br />
-        De joven desarrollé una página web sencilla en HTML para agrupar los partidos de tenis que íbamos haciendo entre
-        amigos y crear una clasificación. Unos años más tarde creé unos formularios PHP con página de resultados para
-        una liga de porras que hacíamos entre los amigos. Actualmente, aparte de seguir formándome como desarrollador
-        front-end, estoy desarrollando una aplicación para IOS y Android, la cual está en fase alpha y apta para probar
-        en la App Store y la Play Store.
+        De joven desarrollé una web sencilla en HTML para agrupar los partidos de tenis que íbamos haciendo entre amigos
+        y crear una clasificación. Unos años más tarde creé una web con formularios PHP con resultados y clasificaciones
+        para una liga de porras que hacíamos entre los amigos. Actualmente, aparte de seguir formándome como
+        Desarrollador Front-End, estoy desarrollando una aplicación para IOS y Android, la cual está en fase alpha y
+        apta para probar en la App Store y la Play Store bajo invitación.
         <br />
         <br />
-        Creo que nunca es tarde para empezar o reinventarse, más aún cuando es algo que siempre me ha apasionado, donde
-        sé que me puedo desenvolver muy bien y donde hay un mundo infinito para aprender y descubrir.
+        Creo que nunca es tarde para empezar o reinventarse. La programación es un mundo que siempre me ha apasionado,
+        donde sé que me puedo desenvolver muy bien y tengo un mundo infinito para aprender y descubrir.
       </>
     ),
   },
@@ -56,7 +56,7 @@ export const aboutMeData = [
     label: 'Intereses y aficiones',
     content: (
       <>
-        Aparte de consultar videos de programación en youtube y mirar artículos de{' '}
+        Aparte de consultar vídeos de programación en Youtube y leer artículos de{' '}
         <Link to="https://medium.com/" target="_blank">
           Medium
         </Link>
