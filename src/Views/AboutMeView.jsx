2ae835e86d8666cf520535f47d2a27b5324d00a4
David Gonzalez <dav6wat@gmail.com>
2025-11-05 15:29:49 +0100

    test 3 data jsx names

--- src/Views/AboutMeView.jsx
+++ src/Views/AboutMeView.jsx
@@ -1,11 +1,11 @@
 import { TextBox } from '../components/TextBox';
-import { aboutMeData } from '../data/AboutMeData.jsx';
+import { AboutMeData } from '../data/AboutData.jsx';
 
 export const AboutMeView = () => {
   return (
     <section className="page-container about-me-container">
       <h2>Conóceme más</h2>
-      {aboutMeData.map((data) => (
+      {AboutMeData.map((data) => (
         <TextBox key={data.label} label={data.label} content={data.content} />
       ))}
     </section>
