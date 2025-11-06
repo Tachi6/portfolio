ab727944f339843ef410319d4f79a8bf5fbc5962
David Gonzalez <dav6wat@gmail.com>
2025-11-06 16:02:57 +0100

    init techsview

--- src/Views/TechsView.jsx
+++ src/Views/TechsView.jsx
@@ -1,14 +1,17 @@
 import { pageTransitions } from '../animations/pageTransition.js';
 // eslint-disable-next-line no-unused-vars
 import { motion } from 'motion/react';
+import { Technology } from '../components/Technology.jsx';
 
 export const TechsView = () => {
   return (
     <motion.section
-      className="page-container about-me-container"
+      className="page-container techs-container"
       initial={pageTransitions.initial}
       animate={pageTransitions.animate}
       exit={pageTransitions.exit}
-    ></motion.section>
+    >
+      <Technology />
+    </motion.section>
   );
 };
---
+++ src/components/Technology.jsx
@@ -0,0 +1,80 @@
+import { ReactLogo } from '../logos/ReactLogo';
+
+export const Technology = () => {
+  return (
+    <>
+      <article className="text-box">
+        <p>
+          <span>
+            <ReactLogo />
+          </span>
+          React
+        </p>
+        <p>Desarrollo web Front End (FSE32-25) por CIFO L'Hospitalet</p>
+        <p>Modulo 1 - HTML y CSS</p>
+        <ul>
+          <li>Configuración del entorno para HTML, CSS y JavaScript</li>
+          <li>HTML5</li>
+          <li>CSS</li>
+          <li>Bootstrap</li>
+        </ul>
+        <p>Modulo 2 - JavaScript</p>
+        <ul>
+          <li>Fundamentos de JavaScript</li>
+          <li>Manipulación del DOM</li>
+          <li>API, Fetch y asincronismo</li>
+          <li>jQuery</li>
+          <li>JavaScript avanzado i fundamentos de TypeScript</li>
+        </ul>
+        <p>Modulo 3 - React</p>
+        <ul>
+          <li>Configuración del entorno para React</li>
+          <li>Componentes, formularios y estado</li>
+          <li>Rutas, Context y tests unitarios</li>
+        </ul>
+        <p>Modulo 4 - React Native</p>
+        <ul>
+          <li>Configuración del entorno para React Native con Expo</li>
+          <li>Componentes propios, API's y React Navigation</li>
+        </ul>
+      </article>
+      <article className="text-box">
+        <p>
+          <span>
+            <ReactLogo />
+          </span>
+          React
+        </p>
+        <p>Front End Web Developer_FOAP 2025 por UOC</p>
+        <p>Modulo 1 - DESARROLLO WEB Y GESTIÓN DE PROYECTOS</p>
+        <ul>
+          <li>Identificación de los elementos de Internet.</li>
+          <li>Aplicación del lenguaje de marcado HTML para maquetar web.</li>
+          <li>Interpretación de los estándares y las directrices de accesibilidad web.</li>
+          <li>Aplicación del lenguaje de hojas de estilo en cascada (CSS) para maquetar web.</li>
+          <li>Análisis de diseños realizados con herramientas de diseño gráfico.</li>
+          <li>Caracterización de los entornos, las fases y herramientas necesarias para realizar proyectos.</li>
+          <li>Relación de los conocimientos de control de versiones y repositorios de código, como GitHub.</li>
+        </ul>
+        <p>Modulo 2 - JAVASCRIPT (ES6)</p>
+        <ul>
+          <li>Caracterización de los fundamentos de la programación Front End: JavaScript.</li>
+          <li>Aplicación de los elementos más avanzados de JavaScript.</li>
+          <li>Interpretación y reescritura de llamadas, datos y código AJAX.</li>
+          <li>Reproducción con código, de prototipos realizados con herramientas de diseño gráfico.</li>
+          <li>Manipulación de código de terceros.</li>
+          <li>Elaboración de documentación técnica y de usuario de lenguaje Javascript.</li>
+        </ul>
+        <p>Modulo 3 - FRAMEWORK DE FRONT-END (VUE.JS)</p>
+        <ul>
+          <li>Distinción de los frameworks de desarrollo Front End web.</li>
+          <li>Identificación de los elementos comunes que aparecen en los diversos frameworks.</li>
+          <li>Utilización de un framework de front-end, en particular, Vue.js.</li>
+          <li>Reproducción con un framework, de prototipos realizados con herramientas de diseño gráfico.</li>
+          <li>Manipulación de código de terceros.</li>
+          <li>Elaboración de documentación técnica y de usuario de un framework.</li>
+        </ul>
+      </article>
+    </>
+  );
+};
