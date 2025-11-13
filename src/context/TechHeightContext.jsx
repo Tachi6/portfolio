23b4fe564b5a08ac1b9bd6fa17d4a28cda69ee3a
David Gonzalez <dav6wat@gmail.com>
2025-11-13 12:25:30 +0100

    change browser icon, clear files

--- src/context/TechHeightContext.jsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react';
-
-export const TechHeightContext = createContext();
-export const CourseInfoHeightContext = createContext();
--- src/context/ThemeContext.jsx
+++
@@ -1,7 +0,0 @@
-// import { createContext, useState } from 'react';
-
-// const ThemeContext = createContext();
-
-// const ThemeProvider = ({children}) => {
-//   const [theme, setTheme] = useState();
-// }
